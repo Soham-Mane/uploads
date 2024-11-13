45fd07638736690e0dda4bb2796ceff7b92616f7
Soham-Mane <116940634+Soham-Mane@users.noreply.github.com>
2024-11-13 16:08:35 +0530

    Update RankP.jsx

--- uploads/frontend/src/pages/RankP.jsx
+++ uploads/frontend/src/pages/RankP.jsx
@@ -9,16 +9,16 @@ import RankingsPage from '../components/Ranking.js';
 
 const RankP = () => {
   return (
-    <div className="h-full w-full">
+    <div className="relative w-full h-[500px] max-w-[1140px] mx-auto bg-cover bg-center">
       <Head />
       <div className='min-h-screen flex flex-col px-8'>
       <div className='w-full  h-fit flex flex-col md:flex-row'>
-          <div className='md:w-3/4'>
+          <div className='md:w-2/3'>
           <div className="container mx-auto p-4">
             <RankingsPage/>
             </div>
             </div>
-            <div className='md:w-1/4'><TrendingNews/><Articles/></div>
+            <div className='md:w-1/3'><TrendingNews/><Articles/></div>
             </div>
             </div>
       <Foot />
