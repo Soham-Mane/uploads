071e3e53d9956c5db953095f40e57eaabe32d567
Soham-Mane <116940634+Soham-Mane@users.noreply.github.com>
2024-12-03 12:24:54 +0530

    Update Home.jsx

--- uploads/frontend/src/pages/Home.jsx
+++ uploads/frontend/src/pages/Home.jsx
@@ -6,7 +6,9 @@ import Home1 from "../components/Home1.jsx";
 const Home = () => {
   return (
     <div className="h-full w-full">
+  <div className='relative w-full  max-w-[1140px] mx-auto bg-cover bg-center'>
       <Head />
+      </div>
       <div>
       <Home1/>
       </div>
