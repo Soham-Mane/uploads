8f94ab7193db5914c318e7c8266239c7d5417565
joshlewis24 <109821868+joshlewis24@users.noreply.github.com>
2024-12-07 00:01:31 +0530

    Update Home.jsx

--- uploads/frontend/src/pages/Home.jsx
+++ uploads/frontend/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Foot from '../components/Foot.jsx';
 import SidePanel from '../components/SidePanel.jsx';
 import DisplaySchedule from '../components/DisplaySchedule.jsx';
 import Home1 from "../components/Home1.jsx";
+import Topnav from '../components/Topnav.jsx';
 const Home = () => {
   return (
     <div className="h-full w-full">
