d9cf9ccf6d45c8e5a0e2bb297ba33f8bb357b217
Soham-Mane <116940634+Soham-Mane@users.noreply.github.com>
2025-01-04 15:44:36 +0530

    Update Home.jsx

--- uploads/frontend/src/pages/Home.jsx
+++ uploads/frontend/src/pages/Home.jsx
@@ -1,27 +1,29 @@
 import React from 'react';
 import Head from '../components/Head.jsx';
 import Foot from '../components/Foot.jsx';
+import SidePanel from '../components/SidePanel.jsx';
+import DisplaySchedule from '../components/DisplaySchedule.jsx';
 import Home1 from "../components/Home1.jsx";
 import Topnav from '../components/Topnav.jsx';
-
 const Home = () => {
   return (
     <div className="h-full w-full flex flex-col">
-      {/* Header Section */}
-      <div className="sticky top-0 z-50 w-full  ">
-        <Topnav />
-        <Head />
-      </div>
-
-      {/* Main Content */}
-      <div className="flex-grow">
-        <Home1 />
-      </div>
-
-      {/* Footer Section */}
-      <Foot />
+    {/* Header Section */}
+    <div className="sticky top-0 z-50 w-full ">
+      <Topnav />
+      <Head />
     </div>
+
+    {/* Main Content */}
+    <div className="flex-grow">
+      <Home1 />
+    </div>
+
+    {/* Footer Section */}
+    <Foot />
+  </div>
   );
-};
+}
 
 export default Home;
+
