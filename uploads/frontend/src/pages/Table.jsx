d5cf90e3e6dbc996d30daacc6e81f4a514351851
joshlewis24 <109821868+joshlewis24@users.noreply.github.com>
2025-01-11 00:30:12 +0530

    Update Table.jsx

--- uploads/frontend/src/pages/Table.jsx
+++ uploads/frontend/src/pages/Table.jsx
@@ -5,12 +5,18 @@ import SidePanel from '../components/SidePanel.jsx';
 import TrendingNews from '../components/TrendingNews.jsx';
 import Articles from '../components/Articles.jsx';
 
-
+import Topnav from '../components/Topnav.jsx';
 
 const Table = () => {
   return (
-    <div className="relative w-full h-[500px] max-w-[1140px] mx-auto bg-cover bg-center">
+    <div className="h-full w-full flex flex-col">
+    {/* Header Section */}
+    <div className="sticky top-0 z-50 w-full  ">
+      <Topnav />
       <Head />
+    </div>
+    <div className=" w-full  max-w-[1140px] mx-auto bg-cover bg-center">
+     
       <div className="flex flex-col md:flex-row p-4 md:p-10 gap-10">
         <div className="flex-grow md:w-2/3">
           <div className="max-w-[1000px] mx-auto">
@@ -22,7 +28,9 @@ const Table = () => {
           <Articles/>
         </div>
       </div>
-      <Foot />
+      
+    </div>
+    <Foot />
     </div>
   )
 }
