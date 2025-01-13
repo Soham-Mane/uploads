c34c1f9a1e34adb8b863b1212c6be1d9b43cf42d
joshlewis24 <109821868+joshlewis24@users.noreply.github.com>
2025-01-13 13:38:27 +0530

    Update Topnav.jsx

--- uploads/frontend/src/components/Topnav.jsx
+++ uploads/frontend/src/components/Topnav.jsx
@@ -18,9 +18,11 @@ const Topnav = () => {
   }).format(currentDate);
 
   return (
-    <div className='flex justify-between px-4 bg-slate-50 w-full'>
+    <div className=' bg-slate-50  '>
+      <div className='max-w-[1140px] xl:mx-auto flex justify-between items-center pr-6 px-4 md:px-0'>
       <div className='flex items-center gap-2 py-1'><div>{formattedDate}</div><div><Layout/></div></div>
-      <div className='flex items-center gap-2 py-1'><FaFacebookF /> <FaInstagram /> <FaYoutube />  <FaLinkedinIn /> <FaSquareXTwitter /><FaReddit /><FaTelegram /> <FaWhatsapp /></div>
+      <div className='flex items-center pr-3 gap-2 py-1'><FaFacebookF /> <FaInstagram /> <FaYoutube />  <FaLinkedinIn /> <FaSquareXTwitter /><FaReddit /><FaTelegram /> <FaWhatsapp /></div>
+      </div>
     </div>
   );
 };
