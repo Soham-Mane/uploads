a6f86f3ab225702b657f81a576a3a0739f331085
joshlewis24 <109821868+joshlewis24@users.noreply.github.com>
2025-02-04 18:43:57 +0530

    Update Table.jsx

--- uploads/frontend/src/pages/Table.jsx
+++ uploads/frontend/src/pages/Table.jsx
@@ -1,38 +1,41 @@
 import Head from '../components/Head.jsx';
 import Foot from '../components/Foot.jsx';
 import DisplayTournament from '../components/DisplayTournament.jsx';
-import SidePanel from '../components/SidePanel.jsx';
 import TrendingNews from '../components/TrendingNews.jsx';
 import Articles from '../components/Articles.jsx';
-
 import Topnav from '../components/Topnav.jsx';
 
 const Table = () => {
   return (
-    <div className="h-full w-full flex flex-col">
-    {/* Header Section */}
-    <div className="sticky top-0 z-50 w-full  ">
-      <Topnav />
-      <Head />
-    </div>
-    <div className=" w-full  max-w-[1140px] mx-auto bg-cover bg-center">
-     
-      <div className="flex flex-col md:flex-row p-4 md:p-10 gap-10">
-        <div className="flex-grow md:w-2/3">
-          <div className="max-w-[1000px] mx-auto">
-            <DisplayTournament/>
+    <div className="min-h-screen w-full flex flex-col">
+      {/* Header Section */}
+      <div className="sticky top-0 z-50 w-full bg-white shadow-md">
+        <Topnav />
+        <Head />
+      </div>
+
+      {/* Main Content */}
+      <div className="w-full max-w-[1140px] mx-auto px-4 sm:px-6 lg:px-8 py-6">
+        <div className="flex flex-col md:flex-row gap-6">
+          {/* Left Side - Tournament Display */}
+          <div className="w-full md:w-2/3">
+            <div className="w-full">
+              <DisplayTournament />
+            </div>
+          </div>
+
+          {/* Right Side - Trending News & Articles */}
+          <div className="w-full md:w-1/3 space-y-6">
+            <TrendingNews />
+            <Articles />
           </div>
         </div>
-        <div className="flex-shrink-0 w-full md:w-1/3">
-          <TrendingNews/>
-          <Articles/>
-        </div>
       </div>
-      
+
+      {/* Footer Section */}
+      <Foot />
     </div>
-    <Foot />
-    </div>
-  )
+  );
 }
 
-export default Table
+export default Table;
